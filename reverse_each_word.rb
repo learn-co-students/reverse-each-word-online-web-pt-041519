085427bf2694514666bd26d422cc36d4e5c7d635
johncban <johncban@gmail.com>
2019-04-22 00:17:45 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def reverse_each_word(string)
   # Source: https://stackoverflow.com/questions/2122380/using-do-block-vs-braces
+  # Source: https://apidock.com/ruby/String/split
   string.split.collect {
     |word| word.reverse
   }.join(" ")
